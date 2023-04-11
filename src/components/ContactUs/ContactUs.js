8d1f44804a5e9ded0e8a1e5bb4609639639986cf
Shrey Batra <shrey.batra@cosmocloud.io>
2023-04-11 17:09:44 +0530

    added m num

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -7,6 +7,7 @@ import Button from "../../common/Button";
 // images
 import email from "../../assets/contact/email.png";
 import location from "../../assets/contact/location.png";
+import phone from "../../assets/contact/phone.png";
 import ContactUsCards from "./ContactUsCards";
 
 const contactData = [
@@ -21,6 +22,12 @@ const contactData = [
     title: "Visit us",
     description: "Come say hello at our office HQ.",
     contactDetails: "Noida, Uttar Pradesh, India - 201301",
+  },
+  {
+    image: phone,
+    title: "Call us",
+    description: "Mon-Fri from 9am to 5pm.",
+    contactDetails: "+91-9818444464",
   },
 ];
 
@@ -87,63 +94,64 @@ function ContactUsComponent() {
       </div>
       <div className="bg-[#1D1B2D] w-[680px] p-10 rounded-lg mobile:rounded-none mobile:py-6 tablet:flex-[1] tablet:px-7 largeMobile:w-[100%] tablet:w-[100%]">
         <div className="mobile:w-[300px] tablet:mx-[auto] tablet:w-[850px] smallTablet:w-[700px] largeMobile:w-[576px]">
-        <div className="flex items-center justify-between tablet:flex-col">
-          <div className="mr-4 tablet:mr-0 tablet:mb-3 tablet:w-[100%]">
-            <p className="mb-3 font-medium text-[18px]">Your name</p>
-            <Input
-              placeholder="What should we call you?"
-              value={formData.name}
+          <div className="flex items-center justify-between tablet:flex-col">
+            <div className="mr-4 tablet:mr-0 tablet:mb-3 tablet:w-[100%]">
+              <p className="mb-3 font-medium text-[18px]">Your name</p>
+              <Input
+                placeholder="What should we call you?"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
+            </div>
+            <div className="tablet:w-[100%]">
+              <p className="mb-3 font-medium text-[18px]">Your email</p>
+              <Input
+                placeholder="john.doe@gmail.com"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="mt-8">
+            <p className="mb-3 font-medium text-[18px]">Subject line</p>
+            <input
+              className={`h-[45px] w-[100%] text-[#95949E] pl-4 pr-4 bg-[#312E45] !outline-none rounded-lg mobile:w-[100%]`}
+              placeholder="What are you looking for?"
+              value={formData.subject}
               onChange={(e) =>
-                setFormData({ ...formData, name: e.target.value })
+                setFormData({ ...formData, subject: e.target.value })
               }
             />
           </div>
-          <div className="tablet:w-[100%]">
-            <p className="mb-3 font-medium text-[18px]">Your email</p>
-            <Input
-              placeholder="john.doe@gmail.com"
-              value={formData.email}
+          <div className="mt-8 mb-10 mobile:mb-6">
+            <p className="mb-3 font-medium text-[18px]">Message</p>
+            <textarea
+              className="text-[#95949E] w-full h-[120px] p-4 bg-[#312E45] !outline-none rounded-lg"
+              placeholder="Write your message here..."
+              value={formData.message}
               onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
+                setFormData({ ...formData, message: e.target.value })
               }
             />
           </div>
-        </div>
-        <div className="mt-8">
-          <p className="mb-3 font-medium text-[18px]">Subject line</p>
-          <input
-            className={`h-[45px] w-[100%] text-[#95949E] pl-4 pr-4 bg-[#312E45] !outline-none rounded-lg mobile:w-[100%]`}
-            placeholder="What are you looking for?"
-            value={formData.subject}
-            onChange={(e) =>
-              setFormData({ ...formData, subject: e.target.value })
-            }
-          />
-        </div>
-        <div className="mt-8 mb-10 mobile:mb-6">
-          <p className="mb-3 font-medium text-[18px]">Message</p>
-          <textarea
-            className="text-[#95949E] w-full h-[120px] p-4 bg-[#312E45] !outline-none rounded-lg"
-            placeholder="Write your message here..."
-            value={formData.message}
-            onChange={(e) =>
-              setFormData({ ...formData, message: e.target.value })
-            }
-          />
-        </div>
-        <Button text="Send" onClick={onSubmit} />
+          <Button text="Send" onClick={onSubmit} />
 
-        {messageSentText && (
-          <p
-            className={`text-left ml-4 text-xs font-medium mt-2 ${messageSentStatus === messageSentStatusValues.SUCCESS
-                ? "text-[#17B169]"
-                : "text-[#EA4882]"
+          {messageSentText && (
+            <p
+              className={`text-left ml-4 text-xs font-medium mt-2 ${
+                messageSentStatus === messageSentStatusValues.SUCCESS
+                  ? "text-[#17B169]"
+                  : "text-[#EA4882]"
               }`}
-          >
-            {messageSentText}
-          </p>
-        )}
-      </div>
+            >
+              {messageSentText}
+            </p>
+          )}
+        </div>
       </div>
       <p className="tablet:block hidden font-medium mobile:w-[330px] mx-auto tablet:text-[28px] tablet:w-[850px] smallTablet:w-[700px] largeMobile:w-[576px]">
         Contact us
